7782bc8adf547b45746ab671ab32e92e0c2de769
JuanchoNY <juanchony@aol.com>
2026-03-20 21:45:16 -0400

    Update ContentView.swift

--- ContentView.swift
+++ ContentView.swift
@@ -1,10 +1,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var events: [Event] = []
+    @State private var newTitle: String = ""
+    
     var body: some View {
         NavigationStack {
-            Text("Calendar")
-                .font(.largeTitle)
-                .navigationTitle("My Calendar")
+            VStack {
+                
+                TextField("New Event", text: $newTitle)
+                    .textFieldStyle(.roundedBorder)
+                    .padding()
+                
+                Button("Add Event") {
+                    let newEvent = Event(title: newTitle, date: Date())
+                    events.append(newEvent)
+                    newTitle = ""
+                }
+                
+                List(events, id: \.title) { event in
+                    Text(event.title)
+                }
+            }
+            .navigationTitle("My Calendar")
         }
-    
+    }
+}
